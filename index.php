e459955bae4f04ece68cf621900638d5d21139fc
enfn2001 <enfn2001@gmail.com>
2012-08-13 20:33:43 -0700

    query string test

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
 		}else{
 			echo   "<p>connection</p>";
 		}
+		//2. seletcion db
+		mysql_select_db('opentutorials');
+		mysql_query("set session character_set_connect=utf8");
+		mysql_query("set session character_set_results=utf8");
+		mysql_query("set session character_set_client=utf8");
+		if(!empty($_GET['id'])){
+			$query = "SELECT * FROM topic where id=".$_GET['id'];
+			$result = mysql_query($query);
+			$topic = mysql_fetch_assoc($result);
+			echo $result.'inner<br/>';
+		}
+		echo $result.'outer';
 	?>
 </body>
 </html>
